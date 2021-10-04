fae96cf7ea00d6b346c24ba0f6fd1ec9a81bd374
Senuka072 <86052269+Senuka072@users.noreply.github.com>
2021-10-04 10:51:30 +0530

    Update password.py

--- password.py
+++ password.py
@@ -43,7 +43,7 @@ print()
 print()
 
 time.sleep(1)
-print(Fore.RED + "     [~]" + Fore.BLUE + "This Tool Can Be Generate A PowerFull Password")
+print(Fore.RED + "     \t[~]" + Fore.BLUE + "This Tool Can Be Generate A PowerFull Password")
 time.sleep(1)
 print(Fore.RED + "\t               [~]" + Fore.BLUE + " Made By Senuka Thisath")
 
@@ -72,7 +72,7 @@ def passwordGen():
     print()
     school = str(input(Fore.BLUE + "[-] Which school do you go? :: " + Fore.RED))
     print()
-    favourite = str(input(Fore.BLUE + "[-] What is your favourite subject? :: "))
+    favourite = str(input(Fore.BLUE + "[-] What is your favourite subject? :: " + Fore.RED))
     print()
     passlegnth = int(input(Fore.BLUE + "[-] How lenght of password do you need (Ex :: 5, 10, 20): " + Fore.RED))
     all = name.upper() + name.lower() + age + school.lower() + school.upper() + favourite.lower() + favourite.upper() + symbols
